a4f93bf7958905302cb5aef1420229d7725483e7
Adam Chambers <adammchambers@gmail.com>
2017-03-04 13:49:04 -0500

    Upgrade to Next 2.0.0, add Drawer with weight class menu

--- pages/index.js
+++ pages/index.js
@@ -6,32 +6,58 @@ import AppBar from 'material-ui/AppBar'
 import {List, ListItem} from 'material-ui/List'
 import Avatar from 'material-ui/Avatar'
 import Divider from 'material-ui/Divider'
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
 import data from '../data.json'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
-export default class extends React.Component {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {open: false}
+
+    this.openDrawer = () => this.setState({open: true})
+    this.closeDrawer = () => this.setState({open: false})
+  }
+
   static async getInitialProps() {
     return {
       items: data
     }
   }
 
+  componentWillMount() {
+    if (typeof window !== 'undefined') injectTapEventPlugin()
+  }
+
   render() {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
         <div>
-          <AppBar title="UFC Rankings" />
+          <AppBar
+            title="UFC Rankings"
+            onLeftIconButtonTouchTap={this.openDrawer}
+          />
           <List>
-            {this.props.items[0].fighters.map((item) => (
+            {this.props.items[0].fighters.map((item, index) => (
               <ListItem
                 leftAvatar={<Avatar>CM</Avatar>}
                 primaryText={item}
                 secondaryText="11 - 1 - 1"
+                key={index}
               />
             ))}
             <Divider inset={true} />
           </List>
+          <Drawer open={this.state.open}>
+            {this.props.items.map((item) => (
+              <MenuItem onTouchTap={this.closeDrawer}>{unescape(item.weightClass)}</MenuItem>
+            ))}
+          </Drawer>
         </div>
       </MuiThemeProvider>
     )
   }
 }
+
+export default App
