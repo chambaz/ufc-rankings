7eaa675be2a4de8e33ae1f2b22092f302bddab75
Adam Chambers <adammchambers@gmail.com>
2017-03-04 10:38:45 -0500

    Install material UI and add theme wrapper to default component

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,8 @@
 import React from 'react'
-export default () => <div>Hello world!</div>
+import ReactDOM from 'react-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+
+export default () =>
+  <MuiThemeProvider>
+    <h1>Hello world</h1>
+  </MuiThemeProvider>
