31d0627da67d97eba480758d10cf0bf7a710f0b3
Adam Chambers <adammchambers@gmail.com>
2017-03-12 12:21:07 -0400

    Add messaging to scrape

--- scrape.js
+++ scrape.js
@@ -4,6 +4,8 @@ const request = require('request')
 const cheerio = require('cheerio')
 const _ = require('lodash')
 
+console.log('Fetching fighters...')
+
 request(ufc('/rankings'), (error, response, html) => {
   if (error || response.statusCode !== 200) {
     return
@@ -30,8 +32,6 @@ request(ufc('/rankings'), (error, response, html) => {
     data.push(listData)
   })
 
-  // console.log(promises)
-
   Promise.all(promises).then((fighters) => {
     fighters.forEach(fighter => {
       const thisFighter = _.find(data, item => {
@@ -41,7 +41,7 @@ request(ufc('/rankings'), (error, response, html) => {
       thisFighter.record = fighter.record
       thisFighter.image = fighter.image
     })
-    console.log(data)
+
     fs.writeFile('data.json', JSON.stringify(data), () => {
       console.log('File saved')
     })
@@ -49,6 +49,7 @@ request(ufc('/rankings'), (error, response, html) => {
 })
 
 function getFighter(weightClass, ranking, url) {
+  console.log(`Fetching ${url}...`)
   return new Promise((resolve, reject) => {
     request(ufc(url), (error, response, html) => {
       if (error || response.statusCode !== 200) {
@@ -56,8 +57,6 @@ function getFighter(weightClass, ranking, url) {
       }
 
       const $ = cheerio.load(html)
-
-      // console.log($('.fighter-record').html())
 
       resolve({
         weightClass,
