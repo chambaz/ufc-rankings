5eed2a8e59cf1bd7b4d75ad6e09145cf0a9ce211
Adam Chambers <adammchambers@gmail.com>
2017-03-12 12:56:32 -0400

    Add avatar and record to view, override document to remove body margin

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,6 @@ import Avatar from 'material-ui/Avatar'
 import Divider from 'material-ui/Divider'
 import Drawer from 'material-ui/Drawer'
 import MenuItem from 'material-ui/MenuItem'
-import Subheader from 'material-ui/Subheader'
 import data from '../data.json'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 import _ from 'lodash'
@@ -44,22 +43,33 @@ class App extends React.Component {
       <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
         <div>
           <AppBar
-            title='UFC Rankings'
+            title={this.state.weightClass}
             onLeftIconButtonTouchTap={this.openDrawer}
           />
           <List>
-            <Subheader>{this.state.weightClass}</Subheader>
             {_.find(this.props.items, item => {
               return item.weightClass === this.state.weightClass
             }).fighters.map((item, index) => (
-              <ListItem
-                leftAvatar={<Avatar>CM</Avatar>}
-                primaryText={item}
-                secondaryText="11 - 1 - 1"
-                key={index}
-              />
+              <div>
+                <ListItem
+                  leftAvatar={(
+                    <Avatar backgroundColor='#fff' style={{overflow: 'hidden'}}>
+                      <div style={{
+                        backgroundImage: `url(${item.image})`,
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        width: 40,
+                        height: 40
+                      }}></div>
+                    </Avatar>
+                  )}
+                  primaryText={item.name}
+                  secondaryText={item.record}
+                  key={index}
+                />
+                <Divider />
+              </div>
             ))}
-            <Divider inset={true} />
           </List>
           <Drawer open={this.state.open}>
             {this.props.items.map((item) => (
