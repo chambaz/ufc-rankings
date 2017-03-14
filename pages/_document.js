71b64919ca881c6ba95ffa78010b4e4d3081c8f9
Adam Chambers <adammchambers@gmail.com>
2017-03-13 20:54:03 -0400

    Add meta description

--- pages/_document.js
+++ pages/_document.js
@@ -12,7 +12,8 @@ export default class MyDocument extends Document {
     return (
      <html>
        <Head>
-        <title>UFC Rankings 👊</title>
+        <title>UFC Rankings</title>
+        <meta name="description" content="UFC rankings app for MMA in pound for pound, heavyweight, light heavyweight, middleweight, welterweight, lightweight, featureweight, bantamweight, flyweight, and strawweight divisions." />
         <meta name="viewport" content="width=device-width, minimum-scale=1.0" />
         <style>{`body { margin: 0 }`}</style>
        </Head>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import AppBar from 'material-ui/AppBar'
